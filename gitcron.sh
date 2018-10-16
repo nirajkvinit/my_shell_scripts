09cd012033468b7f3a0ee2e55500a03c5f610628
Niraj Kumar <nirajkvinit@gmail.com>
2018-10-16 20:46:29 +0530

    Comments updated

--- gitcron.sh
+++ gitcron.sh
@@ -1,3 +1,8 @@
+# Make sure that you have stored your git's credentials previously by issuing following command
+#$ git config credential.helper 'cache --timeout=86400' 
+# The above command will store the credentials for a day. 
+#If you wish you can increase the timeout to suite your purpose.
+
 # Argument 1 - Interval time in minutes
 # Argument 2 - Git Directory Location
 # eg. 	$ ./gitcron.sh 1 /var/www/vawsum  &>/dev/null &
